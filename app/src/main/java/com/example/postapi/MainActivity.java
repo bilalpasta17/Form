7fac28f98b34dc57a7a1a8bbfca546538df99dd8
bilalpasta17 <83414545+bilalpasta17@users.noreply.github.com>
2023-07-21 11:27:48 +0500

    Form

--- app/src/main/java/com/example/postapi/MainActivity.java
+++ app/src/main/java/com/example/postapi/MainActivity.java
@@ -2,20 +2,36 @@ package com.example.postapi;
 
 import static androidx.constraintlayout.helper.widget.MotionEffect.TAG;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
-import android.widget.Checkable;
 import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.RetryPolicy;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.android.material.textfield.TextInputEditText;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.SplittableRandom;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -34,6 +50,7 @@ public class MainActivity extends AppCompatActivity {
     TextView responseTV;
 
     TextInputEditText txtName, txtContactNo, txtEmailAddr, EdDate, EdTime, EdRemarks;
+    String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
 
 
     @Override
@@ -55,12 +72,12 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                Log.d(TAG, "onCheckedChanged: "+isChecked);
-                if(isChecked){
+                Log.d(TAG, "onCheckedChanged: " + isChecked);
+                if (isChecked) {
                     btnSubmit.setEnabled(true);
                     btnSubmit.setAlpha(1);
                     //en
-                }else {
+                } else {
                     btnSubmit.setEnabled(false);
                     btnSubmit.setAlpha(0.5f);
                 }
@@ -71,13 +88,61 @@ public class MainActivity extends AppCompatActivity {
 
             if (checkBox.isChecked()) {
                 validateTxt();
+
             } else {
                 Toast.makeText(this, "Tick the checkbox", Toast.LENGTH_SHORT).show();
             }
-
-            return;
         });
+
+
     }
+
+
+    private void addItemToSheet() {
+        final ProgressDialog dialog = ProgressDialog.show(MainActivity.this, "Adding Item", "Please Wait");
+        String username = txtName.getText().toString().trim();
+        String contactNo = txtContactNo.getText().toString().trim();
+        String emailAddr = txtEmailAddr.getText().toString().trim();
+        String date = EdDate.getText().toString().trim();
+        String time = EdTime.getText().toString().trim();
+        String remarks = EdRemarks.getText().toString().trim();
+
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, "https://script.google.com/macros/s/AKfycbzbX8lzKa1Wq02iK7GMTkuQiNl6tplTzoHzsOOMmwMenrR6ay1j-2wGmTXD28gQxw4O/exec", new com.android.volley.Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                dialog.dismiss();
+                Toast.makeText(MainActivity.this, "" + response, Toast.LENGTH_SHORT).show();
+                Log.d(TAG, "addItemToSheet: " + response.toString());
+
+            }
+        }, error -> {
+
+            dialog.dismiss();
+            Log.d(TAG, "addItemToSheet: " + error.toString());
+        }) {
+            @Nullable
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<>();
+
+                params.put("action", "addItem");
+                params.put("username", username);
+                params.put("contactNo", contactNo);
+                params.put("emailAddr", emailAddr);
+                params.put("date", date);
+                params.put("time", time);
+                params.put("remarks", remarks);
+                return super.getParams();
+            }
+        };
+        int timeOut = 5000;
+        RetryPolicy retryPolicy = new DefaultRetryPolicy(timeOut, 0, DefaultRetryPolicy.DEFAULT_BACKOFF_MULT);
+        stringRequest.setRetryPolicy((retryPolicy));
+
+        RequestQueue queue = Volley.newRequestQueue(MainActivity.this);
+        queue.add(stringRequest);
+    }
+
 
     private void initViews() {
         btnSubmit = findViewById(R.id.btnSubmit);
@@ -99,14 +164,19 @@ public class MainActivity extends AppCompatActivity {
 
                 if (!txtEmailAddr.getText().toString().equals("") || txtEmailAddr.getText().toString().equals(null)) {
 
+
                     if (!EdDate.getText().toString().equals("") || EdDate.getText().toString().equals(null)) {
 
                         if (!EdTime.getText().toString().equals("") || EdTime.getText().toString().equals(null)) {
 
                             if (!EdRemarks.getText().toString().equals("") || EdRemarks.getText().toString().equals(null)) {
+                                String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+.[a-z]+";
+                                if ((txtEmailAddr.getText().toString().matches(emailPattern))) {
+                                    Toast.makeText(MainActivity.this, "All Good", Toast.LENGTH_SHORT).show();
+                                    addItemToSheet();
 
-
-                                Toast.makeText(MainActivity.this, "All Good", Toast.LENGTH_SHORT).show();
+                                } else
+                                    txtEmailAddr.setError("Invalid Email");
 
 
                             } else {
@@ -124,7 +194,7 @@ public class MainActivity extends AppCompatActivity {
 
 
                 } else {
-                    txtEmailAddr.setError("empty not allowed");
+                    txtEmailAddr.setError("check your E-mail Please");
                 }
 
             } else {
@@ -169,4 +239,14 @@ public class MainActivity extends AppCompatActivity {
         });
 
     }
+    // ...
+
+
+    private boolean isValidEmail(String email) {
+        String emailRegex = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$";
+        Pattern pattern = Pattern.compile(emailRegex);
+        Matcher matcher = pattern.matcher(email);
+        return matcher.matches();
+    }
+
 }
